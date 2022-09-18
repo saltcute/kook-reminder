4c64e54dd5e039f1e59e128ddde2f1e3948be530
potatopotat0 <xuanjiap@lolicon.ac.cn>
2022-09-18 12:12:56 -0400

    Add Bot Market support

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import schedule from 'node-schedule';
 import axios from 'axios';
 import got from 'got';
 import * as reminder from './commands/reminder/reminderChannelList'
+import auth from 'configs/auth';
 
 bot.logger.addStream({
     name: "kook-reminder",
@@ -36,3 +37,33 @@ bot.addCommands(reminder_menu);
 bot.connect();
 
 bot.logger.info('System initialization success');
+
+if (auth.useBotMarket) {
+    botMarketStayOnline();
+}
+
+function botMarketStayOnline() {
+    axios({
+        url: 'http://bot.gekj.net/api/v1/online.bot',
+        method: "POST",
+        headers: {
+            uuid: auth.botMarketUUID
+        }
+    }).then((res) => {
+        if (res.data.code == 0) {
+            bot.logger.info(`BotMarket: Successfully updated online status with remote returning: `);
+            bot.logger.info(res.data);
+            setTimeout(botMarketStayOnline, (res.data.data.onTime + 5) * 1000);
+        } else if (res.data.code == -1) {
+            bot.logger.warn(`BotMarket: Failed updating online status with remote returning: `);
+            bot.logger.warn(res.data);
+            bot.logger.warn(`BotMarket: Retries in 30 minutes`);
+            setTimeout(botMarketStayOnline, 30 * 60 * 1000);
+        }
+    }).catch((e) => {
+        bot.logger.warn(`BotMarket: Failed updating online status with remote returning: `);
+        bot.logger.warn(e.message);
+        bot.logger.warn(`BotMarket: Retries in 30 minutes`);
+        setTimeout(botMarketStayOnline, 30 * 60 * 1000);
+    })
+}
